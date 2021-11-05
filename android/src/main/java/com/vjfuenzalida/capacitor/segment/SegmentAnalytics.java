fb9c287439254ea1a79bff95e1cf712ffca59997
Vicente Fuenzalida <vfuenzalida@payflowchile.cl>
2021-11-05 15:53:41 -0300

    feat: implement android plugin

--- android/src/main/java/com/vjfuenzalida/capacitor/segment/SegmentAnalytics.java
+++ android/src/main/java/com/vjfuenzalida/capacitor/segment/SegmentAnalytics.java
@@ -1,11 +1,102 @@
 package com.vjfuenzalida.capacitor.segment;
 
+import android.content.Context;
 import android.util.Log;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.segment.analytics.Analytics;
+import com.segment.analytics.Analytics.Builder;
+import com.segment.analytics.Options;
+import com.segment.analytics.Properties;
+import com.segment.analytics.Traits;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import org.json.JSONException;
 
 public class SegmentAnalytics {
 
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
+    private static final String PLUGIN_TAG = "SegmentAnalytics";
+    public Analytics analytics;
+
+    public void initialize(String writeKey, Boolean trackLifecycleEvents, Boolean recordScreenViews) {
+        Context context = this.getContext();
+        Builder builder = new Analytics.Builder(context, writeKey);
+        if (trackLifecycleEvents) {
+            builder.trackApplicationLifecycleEvents();
+        }
+        if (recordScreenViews) {
+            builder.recordScreenViews();
+        }
+        this.analytics = builder.build();
+    }
+
+    public void identify(String userId, JSObject traits, JSObject options) {
+        this.analytics.identify(userId, makeTraitsFromMap(makeMapFromJSON(traits)), makeOptionsFromJSON(options));
+    }
+
+    public void track(String eventName, JSObject properties, JSObject options) {
+        this.analytics.track(eventName, makePropertiesFromMap(makeMapFromJSON(properties)), makeOptionsFromJSON(options));
+    }
+
+    public void screen(String screenName, JSObject properties, JSObject options) {
+        this.analytics.screen(screenName, makePropertiesFromMap(makeMapFromJSON(properties)), makeOptionsFromJSON(options));
+    }
+
+    public void group(String userId, String groupId, JSObject traits, JSObject options) {
+        this.analytics.group(userId, groupId, makeTraitsFromMap(makeMapFromJSON(traits)), makeOptionsFromJSON(options));
+    }
+
+    public void alias(String newId, JSObject options) {
+        this.analytics.alias(newId, makeOptionsFromJSON(options));
+    }
+
+    public void reset() {
+        this.analytics.reset();
+    }
+
+    private Map<String, Object> makeMapFromJSON(JSObject obj) {
+        Iterator<String> keys = obj.keys();
+        Map<String, Object> map = new HashMap<String, Object>();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            try {
+                Object value = obj.get(key);
+                map.put(key, value);
+            } catch (JSONException e) {
+                Log.d(PLUGIN_TAG, "could not get value for key " + key);
+            }
+        }
+        return map;
+    }
+
+    private Traits makeTraitsFromMap(Map<String, Object> map) {
+        Traits traits = new Traits();
+        traits.putAll(map);
+        return traits;
+    }
+
+    private Properties makePropertiesFromMap(Map<String, Object> map) {
+        Properties properties = new Properties();
+        properties.putAll(map);
+        return properties;
+    }
+
+    private Options makeOptionsFromJSON(JSObject obj) {
+        Options options = new Options();
+        Iterator<String> keys = obj.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            try {
+                boolean enabled = obj.getBool(key);
+                options.setIntegration(key, enabled);
+            } catch (Exception e) {
+                Log.d(PLUGIN_TAG, "could not get boolean for key " + key);
+            }
+        }
+        return options;
     }
 }
--- android/src/main/java/com/vjfuenzalida/capacitor/segment/SegmentAnalyticsPlugin.java
+++ android/src/main/java/com/vjfuenzalida/capacitor/segment/SegmentAnalyticsPlugin.java
@@ -5,18 +5,149 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "SegmentAnalytics")
 public class SegmentAnalyticsPlugin extends Plugin {
 
+    private boolean initialized = false;
     private SegmentAnalytics implementation = new SegmentAnalytics();
 
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    public void initialize(PluginCall call) {
+        if (initialized == true) {
+            call.reject("Segment is already initialized");
+            return;
+        }
+        String writeKey = call.getString("writeKey");
+        if (writeKey == null) {
+            call.reject("Write key is required to initialize plugin");
+            return;
+        }
+        Boolean trackLifecycleEvents = call.getBoolean("trackLifecycleEvents", false);
+        Boolean recordScreenViews = call.getBoolean("recordScreenViews", false);
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+        implementation.initialize(writeKey, trackLifecycleEvents, recordScreenViews);
+        initialized = true;
+        call.resolve();
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    public void identify(PluginCall call) {
+        if (!initialized) {
+            call.reject("Segment is not initialized");
+            return;
+        }
+        String userId = call.getString("userId");
+        if (userId == null) {
+            call.reject("User ID is required for identify");
+            return;
+        }
+        JSObject traits = call.getObject("traits");
+        JSObject options = call.getObject("options");
+
+        implementation.identify(userId, traits, options);
+        call.resolve();
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    public void track(PluginCall call) {
+        if (!initialized) {
+            call.reject("Segment is not initialized");
+            return;
+        }
+        String eventName = call.getString("eventName");
+        if (eventName == null) {
+            call.reject("Event name is not supplied");
+            return;
+        }
+        JSObject properties = call.getObject("properties");
+        JSObject options = call.getObject("options");
+
+        implementation.track(eventName, properties, options);
+        call.resolve();
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    public void screen(PluginCall call) {
+        if (!initialized) {
+            call.reject("Segment is not initialized");
+            return;
+        }
+        String screenName = call.getString("screenName");
+        if (screenName == null) {
+            call.reject("Screen name was not supplied");
+            return;
+        }
+        JSObject properties = call.getObject("properties");
+        JSObject options = call.getObject("options");
+
+        implementation.screen(screenName, properties, options);
+        call.resolve();
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    public void page(PluginCall call) {
+        if (!initialized) {
+            call.reject("Segment is not initialized");
+            return;
+        }
+        String pageName = call.getString("pageName");
+        if (pageName == null) {
+            call.reject("Page name was not supplied");
+            return;
+        }
+        JSObject properties = call.getObject("properties");
+        JSObject options = call.getObject("options");
+
+        implementation.screen(pageName, properties, options);
+        call.resolve();
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    public void group(PluginCall call) {
+        if (!initialized) {
+            call.reject("Segment is not initialized");
+            return;
+        }
+        String userId = call.getString("userId");
+        String groupId = call.getString("groupId");
+        if (userId == null || groupId == null) {
+            call.reject("User ID and Group ID are required");
+            return;
+        }
+        JSObject traits = call.getObject("traits");
+        JSObject options = call.getObject("options");
+
+        implementation.group(userId, groupId, traits, options);
+        call.resolve();
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    public void alias(PluginCall call) {
+        if (!initialized) {
+            call.reject("Segment is not initialized");
+            return;
+        }
+        String newId = call.getString("newId");
+        if (newId == null) {
+            call.reject("New User ID is required");
+            return;
+        }
+        JSObject options = call.getObject("options");
+
+        implementation.group(newId, options);
+        call.resolve();
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    public void reset(PluginCall call) {
+        if (!initialized) {
+            call.reject("Segment is not initialized");
+            return;
+        }
+
+        implementation.reset();
+        call.resolve();
     }
 }
